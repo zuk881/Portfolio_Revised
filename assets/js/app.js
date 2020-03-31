6ed0854c630117c82890d4e49b157f5e9a780207
zuk881 <zuk881@cox.net>
2020-03-30 20:42:24 -0700

    created files and finished basic code for portfolio

---
+++ assets/js/app.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+
+    // JQuery side-nav function call 
+    $(document).ready(function () {
+        $('.sidenav').sidenav();
+    });
+
+   // Owl carousel function
+   var owl = $('.owl-carousel');
+   owl.owlCarousel({
+       items:4,
+       loop:true,
+       margin:10,
+       autoplay:true,
+       autoplayTimeout:2000,
+       autoplayHoverPause:true,
+       responsive : {
+        // breakpoint from 0 up
+        0 : {
+            items:1
+        },
+        // breakpoint from 480 up
+        480 : {
+            itmes:2
+        },
+        // breakpoint from 768 up
+        768 : {
+            items:2
+         },
+         // breakpoint from 1000 up
+        1000 : {
+            items:4
+         }
+    }
+   
+    //owl play/pause buttons
+    });
+   $('.play').on('click',function(){
+       owl.trigger('play.owl.autoplay',[1000])
+   })
+   $('.stop').on('click',function(){
+       owl.trigger('stop.owl.autoplay')
+   })
+  
+});
