6aa83a75685cce233db8780aab2970ead8b93d12
zuk881 <zuk881@cox.net>
2020-06-09 17:31:04 -0700

    updated media queries

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,41 +5,46 @@ $(document).ready(function () {
         $('.sidenav').sidenav();
     });
 
-   // Owl carousel function
-   var owl = $('.owl-carousel');
-   owl.owlCarousel({
-       items:4,
-       loop:true,
-       margin:10,
-       autoplay:true,
-       autoplayTimeout:1800,
-       autoplayHoverPause:true,
-       responsive : {
-        // breakpoint from 0 up
-        0 : {
-            items:1
-        },
-        // breakpoint from 480 up
-        480 : {
-            itmes:1
-        },
-        // breakpoint from 768 up
-        768 : {
-            items:2
-         },
-         // breakpoint from 1000 up
-        1000 : {
-            items:3
-         }
-    }
-   
-    //owl play/pause buttons
+    // Owl carousel function
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+        items: 4,
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        autoplayTimeout: 1800,
+        autoplayHoverPause: true,
+        responsive: {
+            // breakpoint from 0 up
+            0: {
+                items: 1
+            },
+            // breakpoint from 480 up
+            480: {
+                itmes: 1
+            },
+            // breakpoint from 768 up
+            768: {
+                items: 2
+            },
+            // breakpoint from 1000 up
+            1000: {
+                items: 3
+            },
+            // breakpoint from 1200 up
+            1200: {
+                items: 4
+            }
+
+        }
+
+        //owl play/pause buttons
     });
-   $('.play').on('click',function(){
-       owl.trigger('play.owl.autoplay',[1000])
-   })
-   $('.stop').on('click',function(){
-       owl.trigger('stop.owl.autoplay')
-   })
-  
+    $('.play').on('click', function () {
+        owl.trigger('play.owl.autoplay', [1000])
+    })
+    $('.stop').on('click', function () {
+        owl.trigger('stop.owl.autoplay')
+    })
+
 });
